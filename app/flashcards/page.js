f7d8680bbe9ddcf98d6b6acd1e6716403faf63be
AbdallahDalis <dalisabdallah@gmail.com>
2024-08-14 13:55:45 -0500

    first commit

---
+++ app/flashcards/page.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useUser } from '@clerk/clerk-react';
+import { collection, doc, getDoc, setDoc } from 'firebase/firestore';
+import { db } from '../firebase-config';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+export default function Flashcard() {
+  const { isLoaded, isSignedIn, user } = useUser();
+  const [flashcards, setFlashcards] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    async function getFlashcards() {
+      if (!user) return;
+      const docRef = doc(collection(db, 'users'), user.id);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        const collections = docSnap.data().flashcards || [];
+        setFlashcards(collections);
+      } else {
+        await setDoc(docRef, { flashcards: [] });
+      }
+    }
+    getFlashcards();
+  }, [user]);
+
+  const handleCardClick = (id) => {
+    router.push(`/flashcard?id=${id}`);
+  };
+
+  return (
+    <Container maxWidth="md">
+      <Grid container spacing={3} sx={{ mt: 4 }}>
+        {flashcards.map((flashcard, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Card>
+              <CardActionArea onClick={() => handleCardClick(flashcard.name)}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    {flashcard.name}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+}
